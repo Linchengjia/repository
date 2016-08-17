0a43bc5ff200f0e4313f1eb00cb4e40b7d15d5d8
chengxiang.yan <chengxiang.yan@picohood.com>
2016-08-17 10:21:09 +0800

    增加两个文件

---
+++ account.c
@@ -0,0 +1,49 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+//线程运行函数
+
+void* th_fn(void *arg)
+{
+	int distance = (int)arg;
+	int i;
+	for(i = 1; i <= distance; i++){
+		printf("%lx run %d\n", pthread_self(), i);
+		int time = (int) (drand48()*200000);
+		usleep(time);
+	}
+	return (void*)0;
+}
+
+
+
+
+int main(void)
+{
+	int err;
+	pthread_t rabbit, turtle;//定义线程标识符
+	
+	
+	//建立rabbit线程
+	if( (err = pthread_create(&rabbit, NULL, 
+					th_fn, (void*)50)) != 0){
+		perror("pthread_create error");
+	}
+
+	//建立turtle线程	
+	if( (err = pthread_create(&turtle, NULL, 
+					th_fn, (void*)50)) != 0){
+		perror("pthread_create error");
+	}
+	
+	//主控线程调用这个会阻塞，
+	//直到rabbit和turtle线程结束才能运行	
+	pthread_join(rabbit,NULL);
+	pthread_join(rabbit,NULL);
+	//	sleep(10);
+	printf("control thread id : %lx \n", pthread_self());
+	printf("finished!\n");
+
+	return 0;
+}
